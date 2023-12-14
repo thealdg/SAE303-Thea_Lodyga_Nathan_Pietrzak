c4bb5941e2ed43ea369cedac26e2f8971639e855
Théa <131350711+thealdg@users.noreply.github.com>
2023-12-14 18:50:43 +0100

    Update scriptProblems.js

--- SAE/scriptProblems.js
+++ SAE/scriptProblems.js
@@ -1,4 +1,4 @@
-// Fonction pour récupérer les données JSON depuis l'URL
+
 async function fetchJsonData(url) {
     try {
         const response = await fetch(url);
@@ -9,34 +9,27 @@ async function fetchJsonData(url) {
     }
 }
 
-// URL du fichier JSON
 const jsonUrl = 'https://www.cril.univ-artois.fr/~lecoutre/teaching/jssae/code5/results.json';
 
-// Appeler la fonction pour récupérer les données JSON
 fetchJsonData(jsonUrl)
     .then(jsonData => {
         const families = {};
 
-        // Collecter les familles uniques
         jsonData[2].data.forEach(item => {
             const family = item.family;
             families[family] = true;
         });
 
-        // Créer des balises <a> pour chaque famille
         const lesProblemesDiv = document.querySelector('.lesProblemes');
         Object.keys(families).forEach(family => {
             const link = document.createElement('a');
             link.href = 'probleme.html';
             link.textContent = family;
             link.addEventListener('click', function (event) {
-                // Empêcher le comportement par défaut du lien
                 event.preventDefault();
 
-                // Stocker la variable family dans le localStorage
                 localStorage.setItem('selectedFamily', family);
 
-                // Rediriger vers la page "probleme"
                 window.location.href = 'probleme.html';
             });
             lesProblemesDiv.appendChild(link);
